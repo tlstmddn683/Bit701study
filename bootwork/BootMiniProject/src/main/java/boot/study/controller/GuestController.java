3e5ed86ded5174b7b67210970f11e4b1f9d1f92b
tlstmddn683 <tlstmddn683@gmail.com>
2023-04-27 14:33:43 +0900

    commitgit add .

--- bootwork/BootMiniProject/src/main/java/boot/study/controller/GuestController.java
+++ bootwork/BootMiniProject/src/main/java/boot/study/controller/GuestController.java
@@ -1,5 +1,6 @@
 package boot.study.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,44 +26,74 @@ public class GuestController {
 	// 버켓이름 지정
 	private String bucketName = "bit701-bucket-115";// 각자 자기의 버켓이름
 
-	List<MultipartFile> upload;
-	
+	List<String> photoNames = new ArrayList<>();
+
 	@GetMapping("/list")
-	public String list()
+	public String list() 
 	{
 		return "/sub/guest/guestlist";
 	}
+
 	
-	@PostMapping("/upload")
-	@ResponseBody
-	public void upload(List<MultipartFile> upload)
-	{
-		System.out.println("size:"+upload.size());
-		this.upload=upload;
-	}
 	
+//	@PostMapping("/upload")
+//	@ResponseBody
+//	public void upload(List<MultipartFile> upload) {
+//		System.out.println("size:" + upload.size());
+//		System.out.println("filename 0:" + upload.get(0).getOriginalFilename());
+//
+//		photoNames.clear();
+//
+//		for (MultipartFile file : upload) {
+//			// 스토리지에 업로드
+//			String photoname = storageService.uploadFile(bucketName, "guest", file);
+//			System.out.println("name:" + photoname);
+//			// 업로드한 파일명을 db 에 저장
+//			photoNames.add(photoname);
+//
+//		}
+//	}
+
+//	@PostMapping("/insert")
+//	@ResponseBody
+//	public void insertGuest(GuestDto dto) {
+//		// 방명록 데이타부터 db 에 저장
+//		guestService.insertGuest(dto);
+//		System.out.println("guest_idx:" + dto.getGuest_idx());
+//
+//		for (String photoname : photoNames) {
+//			// 업로드한 파일명을 db 에 저장
+//			GuestPhotoDto pdto = new GuestPhotoDto();
+//			pdto.setGuest_idx(dto.getGuest_idx());
+//			pdto.setPhotoname(photoname);
+//			guestService.insertPhoto(pdto);
+//		}
+//	}
+	
+	
+	
+	
+	//insert할때 파일도 같이 업로드
 	@PostMapping("/insert")
 	@ResponseBody
-	public void insertGuest(GuestDto dto)
-	{
-		//방명록 데이터부터 db에 저장합니다
-		guestService.insertGuest(dto);
-		System.out.println("guest_idx:"+dto.getGuest_idx());
+	public void insertGuest(GuestDto dto,List<MultipartFile> upload) {
 		
-		System.out.println("filename:"+upload.get(0).getOriginalFilename());
-		//업로드한 사진이 있는 경우
-		if(!upload.get(0).getOriginalFilename().equals(""))
-		{
-			for(MultipartFile file:upload)
-			{
-				//스토리지에 업로드합니다
-				String photoname=storageService.uploadFile(bucketName, "guest" , file);
-				//업로드한 파일명을 db에 저장합니다
-				GuestPhotoDto pdto=new GuestPhotoDto();
-				pdto.setGuest_idx(dto.getGuest_idx());
-				pdto.setPhotoname(photoname);
-				
-				guestService.insertPhoto(pdto);
+		System.out.println("content:"+dto.getContent());
+		
+		// 방명록 데이타부터 db 에 저장
+		guestService.insertGuest(dto);
+		System.out.println("guest_idx:" + dto.getGuest_idx());
+if(upload!=null) {
+		System.out.println("size:"+upload.size());
+		System.out.println("filename 0:" + upload.get(0).getOriginalFilename());
+		for (MultipartFile file:upload) {
+			//스토리지에 업로드
+			String photoname=storageService.uploadFile(bucketName, "guest", file);
+			// 업로드한 파일명을 db 에 저장
+			GuestPhotoDto pdto = new GuestPhotoDto();
+			pdto.setGuest_idx(dto.getGuest_idx());
+			pdto.setPhotoname(photoname);
+			guestService.insertPhoto(pdto);
 			}
 		}
 	}
--- bootwork/BootMiniProject/src/main/webapp/WEB-INF/guest/guestlist.jsp
+++ bootwork/BootMiniProject/src/main/webapp/WEB-INF/guest/guestlist.jsp
@@ -21,27 +21,100 @@
 $(function(){
 	list();
 	
+	
+	
 	//사진부터업로드
-	$("#photoupload").change(function(){
+	/*$("#photoupload").change(function(){
+		console.dir(this);
+		//file input type 은 한개일지라도 기본이 배열타입으로 넘어옴
+		//그래서 배열의 0번으로 표시한다!!
 		let cnt=$(this)[0].files.length;
 		if(cnt>3){
 			alert("3장까지만 업로드 가능합니다!!");
 			return false;
 		}
+		var form=new FormData();
+		for(i=0;i<$(this)[0].files.length;i++){
+		form.append("upload",$(this)[0].files[i]);//선택한 사진 모두 추가
+		}
+		
+ 		$.ajax({
+			type:"post",
+			dataType:"text",
+			url:"upload",
+			processData:false,
+			contentType:false,
+			data:form,
+			success:function(res){
+		
+			}
+		});
+	});
+	
+	$("#guestadd").click(function(){
+		let content=$("#content").val();
+		let nn=$("#nickname").val();
+		console.log(content);
+		$.ajax({
+			type:"post",
+			dataType:"text",
+			url:"./insert",
+			data:{"content":content,"nickname":nn},
+			success:function(res){
+				//입력값 초기화
+				$("#nickname").val("");
+				$("#content").val("");
+				$("#photoupload").val("");
+				//목록 다시 출력하기
+				list();
+			}
+		});
+	}); */
+	
+	
+	
+	//업로드한 사진들과 데이터를 묶어서 서버에 전송하기
+	$("#guestadd").click(function(){
+		let content=$("#content").val();
+		let cnt=$("#photoupload").val();
+		let nn=$("#nickname").val();
+		var form=new FormData();
+		for(i=0;i<$("#photoupload")[0].files.length;i++){
+		form.append("upload",$("#photoupload")[0].files[i]);
+		}
+		form.append("content",content);
+		form.append("nickname",nn);
+		$.ajax({
+			type:"post",
+			dataType:"text",
+			url:"./insert",
+			processData:false,
+			contentType:false,
+			data:form,
+			success:function(res){
+				//입력값 초기화
+				$("#nickname").val("");
+				$("#content").val("");
+				$("#photoupload").val("");
+				//목록 다시 출력하기
+				list();
+			}
+		});
 	});
 });
 function list()
 {
 	
-	}
+}
 </script>
 </head>
 <body>
-<div style="width: 500px;margin-left:100px;">
-<textarea style="width:80%;height:100px;"class="form-control" id="content">
-</textarea><br>
-<input type="file" id="photoupload" multiple="multiple">
-<button type="button" class="btn btn-sm btn-outline-success" style="margin-left:50px;">등록</button>
+<div style="width:500px;margin-left: 100px;">
+	<input type="text" id="nickname" class="form-control" placeholder="닉네임"  style="width: 200px;">
+	
+	<textarea style="width:80%;height: 100px"  class="form-control" id="content"></textarea>
+	<input type="file" id="photoupload" multiple="multiple">
+	<button type="button" class="btn btn-sm btn-outline-success" id="guestadd">등록</button>
 </div>
 </body>
 </html>
